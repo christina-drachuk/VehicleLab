4f5278bb82c4bbb8cbb6daa9ccff4971417b89cc
yang <71680587+yangster551@users.noreply.github.com>
2022-03-18 08:41:29 -0400

    Create FordFrivolous.java

---
+++ vehicle/FordFrivolous.java
@@ -0,0 +1,41 @@
+package vehicle;
+
+public class FordFrivolous extends GasPoweredCar implements SelfDriving, Flying{
+    /** FordFrivolous has a gas tank of 20 gallons and an MPG of 23.6. */
+    public FordFrivolous(double startingMileage){
+        super("Ford", "Frivolous", startingMileage, 23.6, 30);
+    }
+    /** Defaults mileage to 0. */
+    public FordFrivolous(){
+        super("Ford", "Frivolous", 0, 23.6, 20);  
+    }
+
+    @Override
+    public void fly(double miles){
+        if(miles<0 || miles>getRemainingRange()){
+            throw new IllegalArgumentException();
+        }
+        decreaseFuelLevel(miles * 3);
+    }
+
+    @Override
+    public boolean canFly(double miles){
+        if (miles<0){
+            throw new IllegalArgumentException(); 
+        }
+        if (miles < getFuelLevel()){
+            return true; 
+        }
+        return false; 
+    }
+    
+    @Override
+    public void driveAutonomously(double miles){
+        if(miles<0 || miles>getRemainingRange()){ //can't drive neg or too many miles
+            throw new IllegalArgumentException();
+        }
+
+        decreaseFuelLevel(miles * 2);
+        addMileage(miles); 
+    }
+}
