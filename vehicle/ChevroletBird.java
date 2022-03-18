02149a4efac2d45d6c55b81ec041c5c732fb7a74
yangster551 <71680587+yangster551@users.noreply.github.com>
2022-03-18 10:23:31 -0400

    saved

--- vehicle/ChevroletBird.java
+++ vehicle/ChevroletBird.java
@@ -1,6 +1,6 @@
 package vehicle;
 
-public class ChevroletBird extends ElectricCar implements Flying{
+public class ChevroletBird extends ElectricCar {
     private boolean wingsExtended = false; 
     /** Chevrolet Birds have a 250 mile range on a full charge. They
     start with their wings retracted.*/
@@ -22,22 +22,6 @@ public class ChevroletBird extends ElectricCar implements Flying{
     public void drive(double miles) {
         wingsExtended = false;
         super.drive(miles);
-    }
-    public void fly(double miles) {
-        if(!canFly(miles)) {
-            throw new IllegalArgumentException();
-        }
-        wingsExtended = true;
-        decreaseCharge(miles);
-    }
-    public boolean canFly(double miles) {
-        if (miles < 0) {
-            throw new IllegalArgumentException();
-        }
-        if (miles > getRemainingRange()) {
-            return false;
-        }
-        return true;
     }
     
 }
---
+++ vehicle/ElectricCar.java
@@ -0,0 +1,62 @@
+package vehicle;
+
+public abstract class ElectricCar extends Car {
+    private double milesOnMaxCharge;
+    private double remainingCharge;
+    /** Note: Car begins with a full charge (and thus range).
+    @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
+    public ElectricCar(String make, String model, double startingMileage,
+    double milesOnMaxCharge) {
+        super(make, model, startingMileage);
+        if (milesOnMaxCharge <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.remainingCharge = milesOnMaxCharge;
+        this.milesOnMaxCharge = milesOnMaxCharge;
+    }
+
+    /** Defaults mileage to 0.
+    @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
+    public ElectricCar (String make, String model, double
+    milesOnMaxCharge) {
+        this(make, model, 0, milesOnMaxCharge);
+    }
+
+    /** Drives the full given number of miles.
+    @throws IllegalArgumentException if miles is negative.
+    @throws IllegalArgumentException if miles is too high given the
+    current charge.*/
+    public void drive(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (miles > this.remainingCharge) {
+            throw new IllegalArgumentException();
+        }
+
+        addMileage(miles);
+        decreaseCharge(miles);
+    }
+
+    /** Returns how many more miles the car can currently go without
+    recharging. */
+    public double getRemainingRange() {
+        return remainingCharge;
+    }
+
+    /** Returns how many miles the car could go on a full charge. */
+    public double getMaxRange() {
+        return milesOnMaxCharge;
+    }
+
+    /** Recharges the car to max range capability. */
+    public void recharge() {
+        this.remainingCharge = milesOnMaxCharge;
+    }
+
+    /** Decreases the amount of energy in the battery based by the number
+    of miles passed as an argument. */
+    protected void decreaseCharge(double miles) {
+        this.remainingCharge -= miles;
+    }
+}
